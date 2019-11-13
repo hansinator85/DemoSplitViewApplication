26b19b54671c37a4c4fc834dd3af5945a0370e7a
Johannes Puchberger <johannes.puchberger@fabasoft.com>
2019-11-13 11:30:40 +0100

    update
    update to API 29, use latest build tools, use latest gradle wrapper

--- app/src/main/java/com/fabasoft/demosplitviewapplication/MainActivity.java
+++ app/src/main/java/com/fabasoft/demosplitviewapplication/MainActivity.java
@@ -2,22 +2,27 @@ package com.fabasoft.demosplitviewapplication;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity
+{
   TextView tvDimens;
 
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
+  protected void onCreate(Bundle savedInstanceState)
+  {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    tvDimens = (TextView) findViewById(R.id.tv_dimens);
+    tvDimens = findViewById(R.id.tv_dimens);
     Helper.fillDimensionsTextView(tvDimens, getResources().getConfiguration(), isInMultiWindowMode());
   }
 
   @Override
-  public void onConfigurationChanged(Configuration newConfig) {
+  public void onConfigurationChanged(@NonNull Configuration newConfig)
+  {
     super.onConfigurationChanged(newConfig);
     Helper.fillDimensionsTextView(tvDimens, getResources().getConfiguration(), isInMultiWindowMode());
   }
